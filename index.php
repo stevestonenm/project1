e0d3db9cf061c840abfaa81fe5f4e6cfbdf9d950
Steve Stone <stoneflynm@gmail.com>
2017-10-03 20:29:10 -0600

    added one basic javascript function

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<!-- jQuery   -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+		<!-- javascript -->
+		<script type="text/javascript" src="javascript/script.js"></script>
+		<!-- custom CSS stylesheet -->
+		<link rel="stylesheet" href="css/style.css" type="text/css" />
+
 	</head>
 	<body>
-		<p>Hello World</p>
+		<div id="help" onclick="changeText();">
+			<p>Hello World</p>
+		</div>
 	</body>
 </html>
---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+
+
+function changeText() {
+	document.getElementById("help").innerHTML = "Good Bye World"
+}
